9d9d3a0eed6799e5d6e53997a02ad4bb6885d0e7
David McGrann <dpmcgrann@gmail.com>
2017-12-29 20:31:05 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,13 +1,12 @@
 function forLoop(array) {
   var array = []
-  var i = ''
   for (let i = 0; i < 25; i++) {
     if (i === 1) {
       console.log("I am 1 strange loop.")
     } else  if (true){
       console.log("I am " +i+ " strange loops.")
-    }
-  } return array
+    } return array
+  }
 }
 
 function whileLoop(n) {
