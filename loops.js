61e5128dcd4d12c5349b0a2c6202583ddeca1d73
David McGrann <dpmcgrann@gmail.com>
2017-12-29 19:46:00 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,13 +1,7 @@
+
 function whileLoop(n) {
   let countdown = n
   while (countdown > 0) {
     console.log(--countdown)
   } return ('done')
-}
-
-function forLoop(array) {
-  var array = []
-  for (let i = 0; i < 25; i++) {
-    return ("I am ${i} strange loop${i === 0 ? '' : 's'}.")
-  }
 }
