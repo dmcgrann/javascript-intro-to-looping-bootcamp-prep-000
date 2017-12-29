854df5df6fab25694d8c6c48ad0f45094ec9ee29
David McGrann <dpmcgrann@gmail.com>
2017-12-29 20:46:20 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -3,7 +3,7 @@ function forLoop(array) {
   for (var i = 0; i < 25; i++) {
     if (i === 1) {
       console.log("I am 1 strange loop.")
-    } else  if (i => 0 && i => 25) {
+    } else  if (false) {
       console.log("I am " +i+ " strange loops.")
     }
   } return []
